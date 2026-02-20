951c0b8f0f983a4e38e057998a1df838601e5c9a
Aya Lamouchi <lamouchieya@bizerte.r-iset.tn>
2026-02-20 23:04:54 +0100

    problem

--- smartfuture-frontend/src/main.ts
+++ smartfuture-frontend/src/main.ts
@@ -2,5 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// ✅ IMPORTANT : Importer Zone.js AVANT le bootstrap
+import 'zone.js';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
